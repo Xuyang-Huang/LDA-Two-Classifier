902fb3a70189e9ea6639972cb024e2171cb3c6c3
test <test@test.com>
2021-04-01 14:56:42 +0800

    Update LDA_Two_Classifier.py

--- LDA_Two_Classifier.py
+++ LDA_Two_Classifier.py
@@ -9,7 +9,7 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.datasets import load_iris
+import sklearn.datasets as sk_dataset
 
 
 class LDATwoClassifier:
@@ -17,6 +17,10 @@ class LDATwoClassifier:
 
     Only be used in two class dataset.
     """
+    def __init__(self):
+        self.w = None
+        self.thr = None
+
     def train(self, data, label):
         """Training.
 
@@ -46,6 +50,26 @@ class LDATwoClassifier:
                 item = item[:, np.newaxis]
                 scatters += np.dot((item - mean_tmp), (item - mean_tmp).T)
         self.w = np.dot(np.mat(scatters).I, sample_mean[0] - sample_mean[1])
+        result = self.w.T * data.transpose([1, 0])
+        result = np.array(result)[0]
+        result_index = np.argsort(result)
+        result = result[result_index]
+        label = label[result_index]
+
+        # Find the best threshold to classify.
+        tp_fn = np.sum(label)
+        np_tn = len(label) - tp_fn
+        max_score = 0
+        for i in range(len(result)):
+            tp = np.sum(label[:i])
+            tn = len(label[i:]) - np.sum(label[i:])
+            sensitivity = tp/tp_fn
+            specificity = tn/np_tn
+            score = (sensitivity + specificity)/2
+            if score > max_score:
+                self.thr = result[i]
+                max_score = score
+
 
     def predict(self, data):
         """
@@ -56,7 +80,8 @@ class LDATwoClassifier:
         assert len(data.shape) > 1, f'data shape should be [n, dim].'
 
         result = self.w.T * data.transpose([1, 0])
-        return np.array(result)[0]
+        result = np.array(result)[0] <= self.thr
+        return result
 
     def eval(self, data, label):
         """Evaluate val data and plot result.
@@ -81,32 +106,37 @@ class LDATwoClassifier:
         plt.scatter(np.arange(len(result_0), len(label)), result_1, cmap='g')
         plt.show()
 
+        result = self.w.T * data.transpose([1, 0])
+        result = np.array(result)[0] <= self.thr
+        acc = np.sum(np.equal(result, label)) / len(label)
+        return acc
 
-def prepare_data():
-    iris_dataset = load_iris()
-    label = iris_dataset['target']
-    label = label[np.where(label < 2)]
-    data = iris_dataset['data'][np.where(iris_dataset['target'] < 2)]
+
+def prepare_data(proportion):
+    dataset = sk_dataset.load_breast_cancer()
+    label = dataset['target']
+    data = dataset['data']
+    n_class = len(dataset['target_names'])
 
     shuffle_index = np.arange(len(label))
     np.random.shuffle(shuffle_index)
 
-    train_number = int(0.8 * len(label))
+    train_number = int(proportion * len(label))
     train_index = shuffle_index[:train_number]
     val_index = shuffle_index[train_number:]
     data_train = data[train_index]
     label_train = label[train_index]
     data_val = data[val_index]
     label_val = label[val_index]
-    return (data_train, label_train), (data_val, label_val)
+    return (data_train, label_train), (data_val, label_val), n_class
 
 
 if __name__ == '__main__':
-    train, val = prepare_data()
+    train, val, num_class = prepare_data(0.9)
 
     lda = LDATwoClassifier()
     lda.train(train[0], train[1])
-    lda.eval(val[0], val[1])
+    accuracy = lda.eval(val[0], val[1])
 
     pred = lda.predict(val[0])
     print(pred)
